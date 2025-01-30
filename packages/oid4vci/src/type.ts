442ecc08a9b8afa93ea8771b62bbc60d2244a7bf
Lukas.J.Han <lukas.j.han@gmail.com>
2025-01-30 23:31:47 +0900

    wip: type
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- packages/oid4vci/src/type.ts
+++ packages/oid4vci/src/type.ts
@@ -1,5 +1,93 @@
+import { JsonWebKey } from 'crypto';
+
 export type Oid4VciConfig = {
-  issuerMetadata: {
-    credential_issuer: string;
+  /**
+   * The Credential Issuer's identifier, typically a URL
+   * @example "https://issuer.example.com"
+   */
+  credential_issuer: string;
+
+  /**
+   * List of OAuth 2.0 authorization server URLs
+   * If not provided, the credential_issuer also serves as the authorization server
+   * @example ["https://auth.example.com", "https://auth2.example.com"]
+   * @optional
+   */
+  authorization_servers?: string[];
+
+  /**
+   * The URL of the Credential Issuer's Credential Endpoint
+   * If not provided, use default endpoint: /credential
+   * @example "https://issuer.example.com/credential"
+   * @optional
+   */
+  credential_endpoint?: string;
+
+  /**
+   * The URL of the Credential Issuer's Nonce Endpoint
+   * If not provided, the nonce endpoint is not included
+   * If true, the default nonce endpoint is used: /nonce
+   * @example "https://issuer.example.com/nonce"
+   * @optional
+   */
+  nonce_endpoint?: string | true;
+
+  /**
+   * The URL of the Credential Issuer's Deferred Credential Endpoint
+   * If not provided, the deferred credential endpoint is not included
+   * If true, the default deferred credential endpoint is used: /deferred-credential
+   * @example "https://issuer.example.com/deferred-credential"
+   * @optional
+   */
+  deferred_credential_endpoint?: string | true;
+
+  /**
+   * The URL of the Credential Issuer's Notification Endpoint
+   * If not provided, the notification endpoint is not included
+   * If true, the default notification endpoint is used: /notification
+   * @example "https://issuer.example.com/notification"
+   * @optional
+   */
+  notification_endpoint?: string | true;
+
+  /**
+   * TODO: implement
+   */
+  credential_response_encryption?: never;
+
+  /**
+   * The maximum number of credentials that can be issued in a batch
+   * If not provided, batch issuance is not supported
+   * If 0 or less, batch issuance is disabled
+   * @example 5
+   * @optional
+   */
+  batch_credential_issuance?: number;
+
+  /**
+   * The key used to sign the metadata
+   * If not provided, the metadata is not signed
+   * @example {"alg": "HS256", "secret": "secret"}
+   * @optional
+   */
+  signed_metadata_key?:
+    | {
+        alg: 'HS256';
+        secret: string;
+      }
+    | {
+        alg: 'ES256' | 'ES384' | 'ES512';
+        key: JsonWebKey;
+      };
+
+  display?: {
+    name?: string;
+    locale?: string;
+    logo?: {
+      uri: string;
+      alt_text?: string;
+    };
   };
+
+  credential_configurations_supported?: never;
 };
