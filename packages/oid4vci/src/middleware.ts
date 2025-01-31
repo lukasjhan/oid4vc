60f7995baf777bbb5743d78860cef5d86b3e67ad
Lukas.J.Han <lukas.j.han@gmail.com>
2025-01-31 11:34:54 +0900

    feat: refactor metadata
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- packages/oid4vci/src/middleware.ts
+++ packages/oid4vci/src/middleware.ts
@@ -3,32 +3,33 @@ import { Router, Request, Response, NextFunction } from 'express';
 
 export class Oid4VciMiddleware {
   private router: Router;
-  private readonly config: Oid4VciConfig;
+  private readonly metadata: IssuerMetadata;
 
   constructor(config: Oid4VciConfig) {
-    this.config = this.validateConfig(config);
+    this.metadata = this.validateConfig(config);
     this.router = Router();
-    this.setupRoutes();
+    this.setupRoutes(config);
   }
 
-  private validateConfig(config: Oid4VciConfig) {
+  private validateConfig(config: Oid4VciConfig): IssuerMetadata {
     if (!config.credential_issuer) {
       throw new TypeError('config.issuerMetadata.credential_issuer is missing');
     }
 
-    return config;
+    const metadata: IssuerMetadata = {
+      credential_issuer: config.credential_issuer,
+      credential_endpoint: config.credential_endpoint ?? '',
+      credential_configurations_supported: {},
+    };
+
+    return metadata;
   }
 
-  private setupRoutes() {
+  private setupRoutes(config: Oid4VciConfig) {
     this.router.get(
       '/.well-known/openid-credential-issuer',
       (req: Request, res: Response) => {
-        const config: IssuerMetadata = {
-          credential_issuer: this.config.credential_issuer,
-          credential_endpoint: this.config.credential_endpoint ?? '',
-          credential_configurations_supported: {},
-        };
-        res.set('Cache-Control', 'no-store').json(config);
+        res.set('Cache-Control', 'no-store').json(this.metadata);
       },
     );
   }
